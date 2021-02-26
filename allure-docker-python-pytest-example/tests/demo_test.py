d6ec007861c9c0bce007d99dd78905e0ba6b5e65
Rolandas1369 <rolandaswb@gmail.com>
2021-02-27 00:37:55 +0200

    browsers

--- allure-docker-python-pytest-example/tests/demo_test.py
+++ allure-docker-python-pytest-example/tests/demo_test.py
@@ -1,74 +1,32 @@
-import pytest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import allure
+from selenium import webdriver
 
-@allure.epic('Allure Epic')
-@allure.feature('Demo Feature')
-@allure.story('Passed Example')
-@allure.issue('https://example.org/issue/1')
-@allure.testcase('https://example.org/tms/2')
-def test_passed_scenario():
-    """
-    Passed Scenario
-    """
-    with allure.step("Given I'm on a site"):
-        print('step1')
-    with allure.step("When I enter something on the page"):
-        print('step2')
-    with allure.step("Then I verify is OK"):
-        print('step3')
+options = webdriver.ChromeOptions()
 
-@allure.epic('Allure Epic')
-@allure.feature('Demo Feature')
-@allure.story('Failed Example')
-@allure.issue('https://example.org/issue/3')
-@allure.testcase('https://example.org/tms/4')
-def test_failed_scenario():
-    """
-    Failed Scenario
-    """
-    with allure.step("Given I'm on a site"):
-        print('step1')
-    with allure.step("When I enter something on the page"):
-        print('step2')
-    with allure.step("Then I verify is FAILED"):
-        print('step3')
-        pytest.fail('FAILURE ON PURPOSE')
 
-@allure.epic('Allure Epic')
-@allure.feature('Demo Feature')
-@allure.story('bROKEN Example')
-@allure.issue('https://example.org/issue/5')
-@allure.testcase('https://example.org/tms/6')
-def test_broken_scenario():
-    """
-    Broken Scenario
-    """
-    with allure.step("Given I'm on a site"):
-        print('step1')
-    with allure.step("When I enter something on the page"):
-        print('step2')
-    with allure.step("Then I verify is BROKEN"):
-        print('step3')
-        raise NotImplementedError('NOT IMPLEMENTED')
+capabilities = options.to_capabilities()
 
-@pytest.fixture(autouse=True)
-def hooks():
-    # set up
+        
+capabilities1 = {
+    "browserName": "chrome",
+    "browserVersion": version,
+    "selenoid:options": {
+        # "enableVNC": True,
+        "enableVideo": True
+    }
+}
 
-    yield
-    # tear down
-    f = open('./files/fescobar.png', "rb")
-    image = f.read()
-    allure.attach(
-        image,
-        name = 'image',
-        attachment_type = allure.attachment_type.PNG
-    )
+driver = webdriver.Remote(
+    command_executor="http://45.15.25.151:4444/wd/hub",
+    desired_capabilities=capabilities1)
 
-    f = open('./files/google.mp4', "rb")
-    image = f.read()
-    allure.attach(
-        image,
-        name = 'image',
-        attachment_type = allure.attachment_type.MP4
-    )
+# Welcome to Rocket Software
+@allure.severity(allure.severity_level.NORMAL)
+def test_youtube_like_on_video():
+    driver.get("https://www.youtube.com/")
+    print('Typing')
+    assert True
+
